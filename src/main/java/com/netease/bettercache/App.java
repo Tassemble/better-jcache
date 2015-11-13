bd1d2fec67db1140348778859adbc29017b40de4
陈鸿钦 <hzchenhongqin@corp.netease.com>
2015-11-13 10:13:02 +0800

    init

---
+++ src/main/java/com/netease/bettercache/App.java
@@ -0,0 +1,13 @@
+package com.netease.bettercache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
